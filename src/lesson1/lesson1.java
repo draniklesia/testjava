2069aa5c364ceb40a7e1a714154a19f122569a75
lesia dranik <draniklesia@gmail.com>
2024-01-27 01:09:11 +0200

    add new integer

---
+++ src/lesson1/lesson1.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello AQA-7");
+        int x = 1;
+        System.out.println(x);
+    }
+
+}
+
+
+
+
