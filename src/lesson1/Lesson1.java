cf30a7135c73246076d80104eafdae908bdae15a
lesia dranik <draniklesia@gmail.com>
2024-01-31 01:11:09 +0200

    add lesson1

--- src/lesson1/Lesson1.java
+++ src/lesson1/Lesson1.java
@@ -1,14 +1,12 @@
 package lesson1;
 
-public class lesson1 {
+public class Lesson1 {
     public static void main(String[] args) {
         System.out.println("Hello AQA-7");
+        System.out.println("Hello again AQA-7");
         int x = 1;
         System.out.println(x);
+        System.out.println("Hello again AQA-7");
     }
-
 }
-
-
-
 
