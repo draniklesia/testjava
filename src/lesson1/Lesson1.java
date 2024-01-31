0681ac074d91061649523f47e5f34b857cdf91c2
lesia dranik <draniklesia@gmail.com>
2024-01-31 22:26:33 +0200

    add homework1

--- src/lesson1/Lesson1.java
+++ src/lesson1/Lesson1.java
@@ -1,4 +1,4 @@
-package main.lesson1;
+package lesson1;
 
 public class Lesson1 {
     public static void main(String[] args) {
--- src/lesson2/Task4.java
+++ src/lesson2/Task4.java
@@ -1,4 +1,4 @@
-package main.lesson2.homework1;
+package lesson2;
 
 public class Task4 {
 //  4)
