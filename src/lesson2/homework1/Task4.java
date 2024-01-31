a03521dfcd5ce1dd765ce2f178606a5f200ab72d
lesia dranik <draniklesia@gmail.com>
2024-01-31 21:35:42 +0200

    add homework1

---
+++ src/lesson2/homework1/Task4.java
@@ -0,0 +1,24 @@
+package lesson2.homework1;
+
+public class Task4 {
+//  4)
+//    Coздать две переменные типа int:
+//    int a = 7;
+//    int b = 9;
+//    Поменять между собой значения переменных a и b.
+//    Создавать новые переменные нельзя.
+//    Можно применять только арифметические действия к перемнным a и b.
+//    Вывести в консоль значения переменных a и b после их переопределения.
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 7;
+        b = 9;
+        b = b - a;
+        a = a + b;
+        b = a - b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
